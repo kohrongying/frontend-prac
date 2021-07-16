a075ad6458d60600ae16c3923ba8d0832473f565
rongying <kohrongying@gmail.com>
2021-07-16 17:51:53 +0800

    [ry] Update font sizes, margins and link styling.

--- public/pages/sunnyside/index.js
+++ public/pages/sunnyside/index.js
@@ -10,7 +10,7 @@ const Prac = () => (
 	<section class={styles.grid}>
     <article class={styles.service}>
       <div class={styles.service__text_wrapper}>
-        <h3>Transform your brand</h3>
+        <h2>Transform your brand</h2>
         <p>We are a full-service creative agency specialising in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you</p>
         <a class={styles.link}>LEARN MORE</a>
       </div>
@@ -26,22 +26,22 @@ const Prac = () => (
 
     <article class={styles.service}>
       <div class={styles.service__text_wrapper}>
-        <h3>Stand out to the right audience</h3>
+        <h2>Stand out to the right audience</h2>
         <p>Using a collaborative formula of designers, researchers, photographers, videographers and copywriters, we'll build and extend your brand in digital places.</p>
         <a class={styles.link}>LEARN MORE</a>
       </div>
     </article>
 
-    <article data-bg="graphic">  
+    <article class={styles.service} data-bg="graphic">  
       <div class={styles.service__img__text_wrapper}>
-        <h4>Graphic Design</h4>
+        <h5>Graphic Design</h5>
         <p>Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential client's attention.</p>
       </div>    
     </article>
 
-    <article data-bg="photography">  
+    <article class={styles.service} data-bg="photography">  
       <div class={styles.service__img__text_wrapper}>
-        <h4>Photography</h4>
+        <h5>Photography</h5>
         <p>Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
       </div>    
     </article>
